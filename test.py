f231e6a0f104140c8748272c12ae6c1b508e1677
CTcue <lydia@ctcue.com>
2015-11-27 14:41:24 +0100

    codecs

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import nltk.data
 import frog
+import codecs
 
 text = "CARDIALE VOORGESCHIEDENIS: 1991 myocardinfarct, onderwand. 23-09-2009: ventriculaire tachycardie bij slechte linkerventrikelfunctie en oud infarct, nu geen coronair lijden, op 15-10-2009 implantatie van profylactische 1 kamer ICD. CARDIALE VOORGESCHIEDENIS: 1991 myocardinfarct, onderwand. 23-09-2009: ventriculaire tachycardie bij slechte linkerventrikelfunctie en oud infarct, nu geen coronair lijden, op 15-10-2009 implantatie van (therapeutisch) 1 kamer ICD. Reden van polikliniekbezoek: Familiaire hypercholesterolemie met status na myocardinfarct. 1991: Myocardinfarct op 48-jarige leeftijd bij roken, overgewicht en Familiaire hypercholesterolemie met ernstig belaste familie-anamnese: vader, moeder, twee zussen en een broer overleden allen op een leeftijd van 47-49 jaar aan een myocard infarct en hadden, waar bekend, ook sterk verhoogd cholesterol met ook xanthelasmata arcus lipoïdes en waarschijnlijk xanthomen."
 
