4b0bf9a57d1dfe06f9413fe89eb898cc738a7c26
Lydia Mennes <lydia@ctcue.com>
2016-01-27 11:26:22 +0100

    data

--- test.py
+++ test.py
@@ -1,18 +1,18 @@
 import nltk.data
 import frog
 import codecs
+import docopt
+
+Usage:
+    sample_file_builder <input_file> <output_file>
+
 
 sent_detector = nltk.data.load('tokenizers/punkt/dutch.pickle')
 froggie = frog.Frog(frog.FrogOptions(parser=False), "/etc/frog/frog.cfg")
-counter = 1
+# counter = 1
 
-with codecs.open("results.csv","w","utf-8") as of:
-	with codecs.open("input.txt","r","utf-8") as infile:
-		for text in infile:
-			of.write("text %i"%counter)
-			sentences = sent_detector.tokenize(text.strip())
-			for s in sentences:
-				of.write(s+"\n")
-			for s in sentences:
-				of.write(froggie.process_raw(s))
-			counter+=1
+with codecs.open(output_file,"w","utf-8") as of:
+    with codecs.open(input_file,"r","utf-8") as infile:
+        for line in infile:
+            of.write(froggie.process_raw(s)+"\n")
+            # counter+=1
